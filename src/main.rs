8cbafeabd7d7e30acaa7461c89f57f2674208856
Walfie <walfington@gmail.com>
2018-11-20 17:01:29 -0500

    Get name from argument
    Also set version to alpha

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let name_opt = std::env::args().skip(1).next();
+    let name = name_opt.as_ref().map(String::as_ref).unwrap_or("world");
+
+    println!("Hello, {}!", name);
 }
